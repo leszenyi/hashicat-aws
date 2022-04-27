76975e91aa485e96b0d0789ab049b55aa8a9bacf
Laszlo Eszenyi <laszlo.eszenyi@ni.com>
2022-04-27 21:03:55 +0000

    Added s3 bucket module

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -2,9 +2,10 @@ module "s3_bucket" {
   source = "terraform-aws-modules/s3-bucket/aws"
 
   acl    = "private"
+  bucket_prefix = "laszlo-e"
 
   versioning = {
     enabled = true
   }
-bucket_prefix = "laszlo-e"
+
 }
